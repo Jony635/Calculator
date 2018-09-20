28da49ef5c8142c267d2858374d19153ae69d45c
Jony635 <jony635molina@gmail.com>
2018-09-20 23:15:12 +0200

    3rd commit

--- app/src/main/java/com/example/jonathanmc/calculator/CalculatorActivity.java
+++ app/src/main/java/com/example/jonathanmc/calculator/CalculatorActivity.java
@@ -8,8 +8,10 @@ import android.widget.TextView;
 
 public class CalculatorActivity extends AppCompatActivity {
 
+    ///TODO: WE HAVE TO IMPLEMENT THE VIEW ON SCREEN AND THE REAL NUMBER SEPARATELY.
     private TextView currentnumber;
     private float lastNum = 0f;
+    private boolean negative = false;
 
     private enum operations
     {
@@ -35,53 +37,103 @@ public class CalculatorActivity extends AppCompatActivity {
         Button but = (Button) view;
         String buttonText = but.getText().toString();
         currentnumber.setText(currentnumber.getText() + buttonText);
+        ///TODO: ADD THE NUMBERS TO THE REAL NUMBER WHICH MUST BE IMPLEMENTED.
     }
     public void OnOperator(View view)
     {
-        lastNum = Float.parseFloat(currentnumber.getText().toString());
         Button but = (Button) view;
         String buttonText = but.getText().toString();
 
-        if(buttonText.isEmpty() || actual_operation != operations.NO_OP)
+        if(buttonText.isEmpty())
             return;
-        
+
+        String lastNumberString = currentnumber.getText().toString();
+        boolean alreadyNumbered = !lastNumberString.isEmpty();
+        if(alreadyNumbered)
+        {
+            lastNum = Float.parseFloat(currentnumber.getText().toString());
+            currentnumber.setText("");
+        }
+
         switch(buttonText)
         {
+
+            ///TODO: CONCATENATE OPERATIONS WITHOUT HITTING 'EQUAL' EACH TIME.
             case "*":
             {
+                if(actual_operation != operations.NO_OP || !alreadyNumbered)
+                    return;
                 actual_operation = operations.MULTIPLY;
                 break;
             }
             case ":":
             {
+                if(actual_operation != operations.NO_OP || !alreadyNumbered)
+                    return;
                 actual_operation = operations.DIVIDE;
                 break;
             }
             case "+":
             {
+                if(actual_operation != operations.NO_OP || !alreadyNumbered)
+                    return;
                 actual_operation = operations.ADD;
                 break;
             }
-            case "-": {
-                actual_operation = operations.SUBTRACT;
+            case "-":
+            {
+                if(actual_operation == operations.NO_OP && alreadyNumbered)
+                    actual_operation = operations.SUBTRACT;
+                else if(!negative)
+                {
+                    negative = true;
+
+                    ///TODO: IT SHOWS A 0, BUT REMEMBERS THE NEGATIVE SIGN. MUST BE CORRECTED.
+                    currentnumber.setText(Integer.toString(-0));
+                }
+
                 break;
             }
         }
-
-        currentnumber.setText("");
     }
 
     public void OnResult(View view)
     {
-        Button but = (Button) view;
-        String buttonText = but.getText().toString();
-        currentnumber.setText(currentnumber.getText() + buttonText);
+        float result = 0f;
+        float secondNum = currentnumber.getText().toString().isEmpty() ? 0 : Float.parseFloat(currentnumber.getText().toString());
+        switch(actual_operation)
+        {
+            case ADD:
+            {
+                result = lastNum + secondNum;
+                break;
+            }
+            case SUBTRACT:
+            {
+                result = lastNum - secondNum;
+                break;
+            }
+            case MULTIPLY:
+            {
+                result = lastNum * secondNum;
+                break;
+            }
+            case DIVIDE: {
+                result = lastNum / secondNum;
+                break;
+            }
+        }
+        currentnumber.setText(Float.toString( negative ? -1 * result : result));
+        lastNum = 0f;
+        actual_operation = operations.NO_OP;
+        negative = false;
+        secondNum = 0f;
     }
 
     public void OnDot(View view)
     {
-        Button but = (Button) view;
-        String buttonText = but.getText().toString();
-        currentnumber.setText(currentnumber.getText() + buttonText);
+       // Button but = (Button) view;
+        //String buttonText = but.getText().toString();
+        //currentnumber.setText(currentnumber.getText() + buttonText);
     }
 }
